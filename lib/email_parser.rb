2f7fe0f250d6f518c84eb1bace4e18281fda7b78
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-24 12:01:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,24 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  
+  attr_accessor :name
+  @@all = []
+  def self.all
+    @@all
+  end
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def normalize_name
+    self.name.split(" ").collect{|w| w.capitalize}.join(" ")
+  end
+ 
+  def self.normalize_names
+    self.all.each do |person|
+      person.name = person.normalize_name
+    end
+  end
 end
